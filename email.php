2358e8bec7d3e53fe12a04a295e440d2645734a8
admin <thegajannagaraj@gmail.com>
2015-09-27 21:23:09 -0700

    fixed email bug

--- email.php
+++ email.php
@@ -1,11 +1,7 @@
 <?php
-if (@isset($_SERVER['HTTP_REFERER']) == "http://www.thegajan.github.com/sjcc-mc-stem-tut") {
     $name = $_POST['name'];
     $email = $_POST['email'];
     $message = $_POST['message'];
     $type = $_POST['emailStatus'];
     mail('thegajannagaraj@gmail.com', "TUTORING " . $type . "FROM " . $email, $message);
-} else {
-    header('Location: http://www.thegajan.github.com/sjcc-mc-stem-tut');
-}
 ?>
